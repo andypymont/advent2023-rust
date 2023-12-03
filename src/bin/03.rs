3ce31c6dbdb9f58fffee49f2f4492532e213b27c
Andy Pymont <andy@andypymont.co.uk>
2023-12-03 13:31:27 +0000

    Day 3 Part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -28,6 +28,22 @@ impl Part {
     }
 }
 
+#[derive(Debug, PartialEq)]
+enum Gear {
+    Single(u32),
+    Valid(u32),
+    Overloaded(u32),
+}
+
+impl Gear {
+    fn connect(&self, part_no: u32) -> Self {
+        match self {
+            Self::Single(ratio) => Self::Valid(ratio * part_no),
+            Self::Valid(ratio) | Self::Overloaded(ratio) => Self::Overloaded(*ratio),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Schematic {
     symbols: HashMap<Point, char>,
@@ -37,12 +53,35 @@ struct Schematic {
 impl Schematic {
     fn part_numbers(&self) -> impl Iterator<Item = u32> + '_ {
         self.parts.iter().filter_map(|part| {
-            if part.adjacent_points().iter().any(|pt| self.symbols.contains_key(pt)) {
+            if part
+                .adjacent_points()
+                .iter()
+                .any(|pt| self.symbols.contains_key(pt))
+            {
                 Some(part.number)
             } else {
                 None
             }
         })
+    }
+
+    fn gears(&self) -> HashMap<Point, Gear> {
+        let mut gears = HashMap::new();
+
+        for part in &self.parts {
+            for point in part.adjacent_points() {
+                if let Some('*') = self.symbols.get(&point) {
+                    gears
+                        .entry(point)
+                        .and_modify(|gear: &mut Gear| {
+                            *gear = gear.connect(part.number);
+                        })
+                        .or_insert_with(|| Gear::Single(part.number));
+                }
+            }
+        }
+
+        gears
     }
 }
 
@@ -72,7 +111,10 @@ impl FromStr for Schematic {
                     }
                 } else {
                     if let Some(began_x) = number_began {
-                        parts.insert(Part { number, position: Point(began_x, y) });
+                        parts.insert(Part {
+                            number,
+                            position: Point(began_x, y),
+                        });
                         number_began = None;
                         number = 0;
                     }
@@ -83,11 +125,14 @@ impl FromStr for Schematic {
             }
 
             if let Some(began_x) = number_began {
-                parts.insert(Part { number, position: Point(began_x, y) });
+                parts.insert(Part {
+                    number,
+                    position: Point(began_x, y),
+                });
             }
         }
 
-        Ok(Schematic { symbols, parts})
+        Ok(Schematic { symbols, parts })
     }
 }
 
@@ -101,8 +146,24 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 #[must_use]
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    if let Ok(schematic) = input.parse::<Schematic>() {
+        Some(
+            schematic
+                .gears()
+                .iter()
+                .filter_map(|(_, gear)| {
+                    if let Gear::Valid(ratio) = gear {
+                        Some(ratio)
+                    } else {
+                        None
+                    }
+                })
+                .sum(),
+        )
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -119,31 +180,75 @@ mod tests {
         symbols.insert(Point(6, 9), '*');
 
         let mut parts = HashSet::new();
-        parts.insert(Part { number: 467, position: Point(1, 1) });
-        parts.insert(Part { number: 114, position: Point(6, 1) });
-        parts.insert(Part { number: 35, position: Point(3, 3) });
-        parts.insert(Part { number: 633, position: Point(7, 3) });
-        parts.insert(Part { number: 617, position: Point(1, 5) });
-        parts.insert(Part { number: 58, position: Point(8, 6) });
-        parts.insert(Part { number: 592, position: Point(3, 7) });
-        parts.insert(Part { number: 755, position: Point(7, 8) });
-        parts.insert(Part { number: 664, position: Point(2, 10) });
-        parts.insert(Part { number: 598, position: Point(6, 10) });
+        parts.insert(Part {
+            number: 467,
+            position: Point(1, 1),
+        });
+        parts.insert(Part {
+            number: 114,
+            position: Point(6, 1),
+        });
+        parts.insert(Part {
+            number: 35,
+            position: Point(3, 3),
+        });
+        parts.insert(Part {
+            number: 633,
+            position: Point(7, 3),
+        });
+        parts.insert(Part {
+            number: 617,
+            position: Point(1, 5),
+        });
+        parts.insert(Part {
+            number: 58,
+            position: Point(8, 6),
+        });
+        parts.insert(Part {
+            number: 592,
+            position: Point(3, 7),
+        });
+        parts.insert(Part {
+            number: 755,
+            position: Point(7, 8),
+        });
+        parts.insert(Part {
+            number: 664,
+            position: Point(2, 10),
+        });
+        parts.insert(Part {
+            number: 598,
+            position: Point(6, 10),
+        });
 
         Schematic { symbols, parts }
     }
 
     #[test]
     fn test_parse_schematic() {
-        let parsed: Schematic = advent_of_code::template::read_file("examples", DAY).parse().unwrap();
+        let parsed: Schematic = advent_of_code::template::read_file("examples", DAY)
+            .parse()
+            .unwrap();
         let expected = example_schematic();
 
-        assert_eq!(parsed.symbols.len(), expected.symbols.len(), "number of symbols matches expected");
+        assert_eq!(
+            parsed.symbols.len(),
+            expected.symbols.len(),
+            "number of symbols matches expected"
+        );
         for (point, symbol) in expected.symbols {
-            assert_eq!(parsed.symbols.get(&point), Some(&symbol), "check for {symbol} at {point:?}");
+            assert_eq!(
+                parsed.symbols.get(&point),
+                Some(&symbol),
+                "check for {symbol} at {point:?}"
+            );
         }
 
-        assert_eq!(parsed.parts.len(), expected.parts.len(), "number of parts matches expected");
+        assert_eq!(
+            parsed.parts.len(),
+            expected.parts.len(),
+            "number of parts matches expected"
+        );
         for part in expected.parts {
             assert!(parsed.parts.contains(&part), "check for {part:?}");
         }
@@ -151,7 +256,10 @@ mod tests {
 
     #[test]
     fn test_adjacent_points_single_digit() {
-        let part = Part { number: 4, position: Point(1, 1) };
+        let part = Part {
+            number: 4,
+            position: Point(1, 1),
+        };
         let adjacent = part.adjacent_points();
         assert_eq!(adjacent.len(), 8, "8 adjacent points");
         assert!(adjacent.contains(&Point(0, 0)), "NW");
@@ -166,7 +274,10 @@ mod tests {
 
     #[test]
     fn test_adjacent_points_three_digits() {
-        let part = Part { number: 437, position: Point(1, 1) };
+        let part = Part {
+            number: 437,
+            position: Point(1, 1),
+        };
         let adjacent = part.adjacent_points();
         assert_eq!(adjacent.len(), 12, "12 adjacent points");
         assert!(adjacent.contains(&Point(0, 0)), "NW");
@@ -187,7 +298,11 @@ mod tests {
     fn test_part_numbers() {
         let parts: Vec<u32> = example_schematic().part_numbers().collect();
         assert_eq!(parts.len(), 8, "8 valid part numbers (next to symbols)");
-        assert_eq!(parts.iter().sum::<u32>(), 4361, "valid part numbers sum to 4361");
+        assert_eq!(
+            parts.iter().sum::<u32>(),
+            4361,
+            "valid part numbers sum to 4361"
+        );
     }
 
     #[test]
@@ -197,8 +312,17 @@ mod tests {
     }
 
     #[test]
+    fn test_gears() {
+        let gears = example_schematic().gears();
+        assert_eq!(gears.len(), 3);
+        assert_eq!(gears.get(&Point(4, 2)), Some(&Gear::Valid(16345)));
+        assert_eq!(gears.get(&Point(4, 5)), Some(&Gear::Single(617)));
+        assert_eq!(gears.get(&Point(6, 9)), Some(&Gear::Valid(451490)));
+    }
+
+    #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
