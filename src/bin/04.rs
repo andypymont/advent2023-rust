8fd5af08f015e2c29535b24994bd0644e25547d9
Andy Pymont <andy@andypymont.co.uk>
2023-12-04 15:17:06 +0000

    Day 4: Remove Vec collection of Scratchcards for faster performance

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -56,42 +56,34 @@ impl FromStr for Scratchcard {
     }
 }
 
-fn parse_scratchcards(input: &str) -> Result<Vec<Scratchcard>, ParseScratchcardError> {
-    let mut scratchcards = Vec::new();
-
-    for line in input.lines() {
-        let scratchcard = line.parse()?;
-        scratchcards.push(scratchcard);
-    }
-
-    Ok(scratchcards)
-}
-
 #[must_use]
 pub fn part_one(input: &str) -> Option<u32> {
-    if let Ok(scratchcards) = parse_scratchcards(input) {
-        Some(scratchcards.iter().map(Scratchcard::score).sum())
-    } else {
-        None
+    let mut total_score = 0;
+    for scratchcard in input.lines().map(Scratchcard::from_str) {
+        match scratchcard {
+            Ok(scratchcard) => total_score += scratchcard.score(),
+            Err(_) => return None,
+        }
     }
+    Some(total_score)
 }
 
 #[must_use]
 pub fn part_two(input: &str) -> Option<u32> {
-    if let Ok(scratchcards) = parse_scratchcards(input) {
-        let mut copies = vec![1; scratchcards.len()];
-        let mut total = 0;
-        for (ix, card) in scratchcards.iter().enumerate() {
-            total += copies[ix];
-            let matches = card.matches();
-            for card in ix + 1..ix + 1 + matches {
-                copies[card] += copies[ix];
+    let mut copies = [1; 190];
+    let mut total = 0;
+    for (ix, scratchcard) in input.lines().map(Scratchcard::from_str).enumerate() {
+        match scratchcard {
+            Ok(scratchcard) => {
+                total += copies[ix];
+                for card in (ix + 1)..(ix + 1 + scratchcard.matches()) {
+                    copies[card] += copies[ix];
+                }
             }
+            Err(_) => return None,
         }
-        Some(total)
-    } else {
-        None
     }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -106,39 +98,6 @@ mod tests {
                 winners: vec![41, 48, 83, 86, 17],
                 numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
             })
-        );
-    }
-
-    #[test]
-    fn test_parse_scratchcards() {
-        assert_eq!(
-            parse_scratchcards(&advent_of_code::template::read_file("examples", DAY)),
-            Ok(vec![
-                Scratchcard {
-                    winners: vec![41, 48, 83, 86, 17],
-                    numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
-                },
-                Scratchcard {
-                    winners: vec![13, 32, 20, 16, 61],
-                    numbers: vec![61, 30, 68, 82, 17, 32, 24, 19],
-                },
-                Scratchcard {
-                    winners: vec![1, 21, 53, 59, 44],
-                    numbers: vec![69, 82, 63, 72, 16, 21, 14, 1],
-                },
-                Scratchcard {
-                    winners: vec![41, 92, 73, 84, 69],
-                    numbers: vec![59, 84, 76, 51, 58, 5, 54, 83],
-                },
-                Scratchcard {
-                    winners: vec![87, 83, 26, 28, 32],
-                    numbers: vec![88, 30, 70, 12, 93, 22, 82, 36],
-                },
-                Scratchcard {
-                    winners: vec![31, 18, 13, 56, 72],
-                    numbers: vec![74, 77, 10, 23, 35, 67, 36, 11],
-                },
-            ]),
         );
     }
 
