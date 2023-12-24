5115dad5673b9ff170d19afced39745b747b69b5
Andy Pymont <andy@andypymont.co.uk>
2023-12-24 20:48:16 +0000

    Day 21 Part 2

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -4,6 +4,9 @@ use std::str::FromStr;
 advent_of_code::solution!(21);
 
 #[derive(Debug, PartialEq)]
+struct ParseGridError;
+
+#[derive(Debug, PartialEq)]
 enum Direction {
     North,
     East,
@@ -18,357 +21,259 @@ const COMPASS: [Direction; 4] = [
     Direction::West,
 ];
 
-#[derive(Debug, Eq, Hash, PartialEq)]
-struct RepeatedGridPosition {
-    x: i32,
-    y: i32,
-    position: usize,
-}
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Point(i64, i64);
 
-#[derive(Debug, PartialEq)]
-struct Garden {
-    height: usize,
-    width: usize,
-    start: usize,
-    rocks: Vec<bool>,
-}
-
-impl Garden {
-    fn initial_position(&self) -> HashSet<usize> {
-        let mut positions = HashSet::new();
-        positions.insert(self.start);
-        positions
-    }
-
-    fn initial_position_on_repeated_grid(&self) -> HashSet<RepeatedGridPosition> {
-        let mut positions = HashSet::new();
-        positions.insert(RepeatedGridPosition {
-            x: 0,
-            y: 0,
-            position: self.start,
-        });
-        positions
-    }
-
-    fn step_in_direction(&self, position: usize, direction: &Direction) -> Option<usize> {
-        let row = position / self.width;
-        let col = position % self.width;
-
+impl Point {
+    fn neighbour_in_direction(self, direction: &Direction) -> Self {
         match direction {
-            Direction::North => position.checked_sub(self.width),
-            Direction::East => {
-                if (col + 1) < self.width {
-                    Some(position + 1)
-                } else {
-                    None
-                }
-            }
-            Direction::South => {
-                if (row + 1) < self.height {
-                    Some(position + self.width)
-                } else {
-                    None
-                }
-            }
-            Direction::West => {
-                if col > 0 {
-                    Some(position - 1)
-                } else {
-                    None
-                }
-            }
+            Direction::North => Self(self.0 - 1, self.1),
+            Direction::East => Self(self.0, self.1 + 1),
+            Direction::South => Self(self.0 + 1, self.1),
+            Direction::West => Self(self.0, self.1 - 1),
         }
     }
 
-    fn step_in_direction_on_repeated_grid(
-        &self,
-        from: &RepeatedGridPosition,
-        direction: &Direction,
-    ) -> RepeatedGridPosition {
-        let row = from.position / self.width;
-        let col = from.position % self.width;
-
-        match direction {
-            Direction::North => {
-                if row > 0 {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y,
-                        position: from.position - self.width,
-                    }
-                } else {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y - 1,
-                        position: ((self.height - 1) * self.width) + col,
-                    }
-                }
-            }
-            Direction::East => {
-                if (col + 1) < self.width {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y,
-                        position: from.position + 1,
-                    }
-                } else {
-                    RepeatedGridPosition {
-                        x: from.x + 1,
-                        y: from.y,
-                        position: (row * self.width),
-                    }
-                }
-            }
-            Direction::South => {
-                if (row + 1) < self.height {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y,
-                        position: from.position + self.width,
-                    }
-                } else {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y + 1,
-                        position: col,
-                    }
-                }
-            }
-            Direction::West => {
-                if col > 0 {
-                    RepeatedGridPosition {
-                        x: from.x,
-                        y: from.y,
-                        position: from.position - 1,
-                    }
-                } else {
-                    RepeatedGridPosition {
-                        x: from.x - 1,
-                        y: from.y,
-                        position: (self.width * row) + self.width - 1,
-                    }
-                }
-            }
-        }
-    }
-
-    fn step_from_positions(&self, positions: &HashSet<usize>) -> HashSet<usize> {
-        let mut after = HashSet::new();
-
-        for position in positions {
-            for direction in COMPASS {
-                if let Some(dest) = self.step_in_direction(*position, &direction) {
-                    if !self.rocks[dest] {
-                        after.insert(dest);
-                    }
-                }
-            }
-        }
-
-        after
-    }
-
-    fn step_from_positions_on_repeated_grid(
-        &self,
-        positions: &HashSet<RepeatedGridPosition>,
-    ) -> HashSet<RepeatedGridPosition> {
-        let mut after = HashSet::new();
-
-        for position in positions {
-            for direction in COMPASS {
-                let dest = self.step_in_direction_on_repeated_grid(position, &direction);
-                if !self.rocks[dest.position] {
-                    after.insert(dest);
-                }
-            }
-        }
-
-        after
-    }
-
-    fn total_reachable_in_steps(&self, steps: usize) -> usize {
-        let mut positions = self.initial_position();
-        for _ in 0..steps {
-            positions = self.step_from_positions(&positions);
-        }
-        positions.len()
-    }
-
-    fn total_reachable_on_repeated_grid(&self, steps: usize) -> usize {
-        let mut positions = self.initial_position_on_repeated_grid();
-        for _ in 0..steps {
-            positions = self.step_from_positions_on_repeated_grid(&positions);
-        }
-        positions.len()
+    fn neighbours(self) -> impl Iterator<Item = Self> {
+        COMPASS
+            .iter()
+            .map(move |direction| self.neighbour_in_direction(direction))
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct ParseGardenError;
+struct Grid {
+    start: Point,
+    size: i64,
+    rocks: HashSet<Point>,
+}
 
-impl FromStr for Garden {
-    type Err = ParseGardenError;
+impl Grid {
+    fn initial_state(&self) -> HashSet<Point> {
+        let mut state = HashSet::new();
+        state.insert(self.start);
+        state
+    }
+
+    fn point_in_bounds(&self, point: &Point) -> bool {
+        point.0 >= 0 && point.0 < self.size && point.1 >= 0 && point.1 < self.size
+    }
+
+    fn reachable_point(&self, point: &Point, repeating_grid: bool) -> bool {
+        if repeating_grid {
+            let bounded_point = Point(point.0.rem_euclid(self.size), point.1.rem_euclid(self.size));
+            !self.rocks.contains(&bounded_point)
+        } else {
+            self.point_in_bounds(point) & !self.rocks.contains(point)
+        }
+    }
+
+    fn step(&self, state: &HashSet<Point>, repeating_grid: bool) -> HashSet<Point> {
+        state
+            .iter()
+            .flat_map(|pt| pt.neighbours())
+            .filter(|pt| self.reachable_point(pt, repeating_grid))
+            .collect()
+    }
+
+    fn reachable_in_steps(&self, steps: usize, repeating_grid: bool) -> usize {
+        let mut reached = self.initial_state();
+        for _ in 0..steps {
+            reached = self.step(&reached, repeating_grid);
+        }
+        reached.len()
+    }
+}
+
+impl FromStr for Grid {
+    type Err = ParseGridError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let mut rocks = Vec::new();
-        let mut start: Result<usize, Self::Err> = Err(ParseGardenError);
-
-        let mut width = 0;
+        let mut rocks = HashSet::new();
         let mut height = 0;
+        let mut width = 0;
+        let mut start = Err(ParseGridError);
 
         for (row, line) in input.lines().enumerate() {
+            let row = i64::try_from(row).map_err(|_| ParseGridError)?;
             height = row + 1;
             width = width.max(line.len());
 
             for (col, ch) in line.chars().enumerate() {
+                let col = i64::try_from(col).map_err(|_| ParseGridError)?;
+                let pt = Point(row, col);
                 match ch {
-                    '#' => rocks.push(true),
-                    'S' => {
-                        rocks.push(false);
-                        start = Ok((row * width) + col);
+                    'S' => start = Ok(pt),
+                    '#' => {
+                        rocks.insert(pt);
                     }
-                    '.' => rocks.push(false),
-                    _ => return Err(ParseGardenError),
+                    '.' => (),
+                    _ => return Err(ParseGridError),
                 }
             }
         }
 
-        let start = start?;
-        Ok(Self {
-            height,
-            width,
-            start,
-            rocks,
-        })
+        let width = i64::try_from(width).map_err(|_| ParseGridError)?;
+
+        if height == 0 || width == 0 || height != width {
+            Err(ParseGridError)
+        } else {
+            let start = start?;
+            Ok(Self {
+                rocks,
+                size: height,
+                start,
+            })
+        }
     }
 }
 
 #[must_use]
 pub fn part_one(input: &str) -> Option<usize> {
-    if let Ok(garden) = Garden::from_str(input) {
-        Some(garden.total_reachable_in_steps(64))
+    if let Ok(grid) = Grid::from_str(input) {
+        Some(grid.reachable_in_steps(64, false))
     } else {
         None
     }
 }
 
+fn quadratic_formula(sample1: usize, sample2: usize, sample3: usize, n: usize) -> usize {
+    let a = (sample3 - (2 * sample2) + sample1) / 2;
+    let b = sample2 - sample1 - a;
+    let c = sample1;
+    (a * n.pow(2)) + (b * n) + c
+}
+
+const GOAL: usize = 26_501_365;
+
 #[must_use]
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    if let Ok(grid) = Grid::from_str(input) {
+        let Ok(size) = usize::try_from(grid.size) else {
+            return None;
+        };
+        let edge = size / 2;
+
+        let sample1 = grid.reachable_in_steps(edge, true);
+        let sample2 = grid.reachable_in_steps(edge + size, true);
+        let sample3 = grid.reachable_in_steps(edge + (2 * size), true);
+
+        Some(quadratic_formula(
+            sample1,
+            sample2,
+            sample3,
+            (GOAL - edge) / size,
+        ))
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn position(row: usize, col: usize) -> usize {
-        (row * 11) + col
-    }
-
-    fn example_garden() -> Garden {
-        let mut rocks = vec![false; 11 * 11];
-        rocks[position(1, 5)] = true;
-        rocks[position(1, 6)] = true;
-        rocks[position(1, 7)] = true;
-        rocks[position(1, 9)] = true;
-        rocks[position(2, 1)] = true;
-        rocks[position(2, 2)] = true;
-        rocks[position(2, 3)] = true;
-        rocks[position(2, 5)] = true;
-        rocks[position(2, 6)] = true;
-        rocks[position(2, 9)] = true;
-        rocks[position(3, 2)] = true;
-        rocks[position(3, 4)] = true;
-        rocks[position(3, 8)] = true;
-        rocks[position(4, 4)] = true;
-        rocks[position(4, 6)] = true;
-        rocks[position(5, 1)] = true;
-        rocks[position(5, 2)] = true;
-        rocks[position(5, 6)] = true;
-        rocks[position(5, 7)] = true;
-        rocks[position(5, 8)] = true;
-        rocks[position(5, 9)] = true;
-        rocks[position(6, 1)] = true;
-        rocks[position(6, 2)] = true;
-        rocks[position(6, 5)] = true;
-        rocks[position(6, 9)] = true;
-        rocks[position(7, 7)] = true;
-        rocks[position(7, 8)] = true;
-        rocks[position(8, 1)] = true;
-        rocks[position(8, 2)] = true;
-        rocks[position(8, 4)] = true;
-        rocks[position(8, 6)] = true;
-        rocks[position(8, 7)] = true;
-        rocks[position(8, 8)] = true;
-        rocks[position(8, 9)] = true;
-        rocks[position(9, 1)] = true;
-        rocks[position(9, 2)] = true;
-        rocks[position(9, 5)] = true;
-        rocks[position(9, 6)] = true;
-        rocks[position(9, 8)] = true;
-        rocks[position(9, 9)] = true;
-
-        Garden {
-            height: 11,
-            width: 11,
-            start: position(5, 5),
+    fn example_grid() -> Grid {
+        let mut rocks = HashSet::new();
+        rocks.insert(Point(1, 5));
+        rocks.insert(Point(1, 6));
+        rocks.insert(Point(1, 7));
+        rocks.insert(Point(1, 9));
+        rocks.insert(Point(2, 1));
+        rocks.insert(Point(2, 2));
+        rocks.insert(Point(2, 3));
+        rocks.insert(Point(2, 5));
+        rocks.insert(Point(2, 6));
+        rocks.insert(Point(2, 9));
+        rocks.insert(Point(3, 2));
+        rocks.insert(Point(3, 4));
+        rocks.insert(Point(3, 8));
+        rocks.insert(Point(4, 4));
+        rocks.insert(Point(4, 6));
+        rocks.insert(Point(5, 1));
+        rocks.insert(Point(5, 2));
+        rocks.insert(Point(5, 6));
+        rocks.insert(Point(5, 7));
+        rocks.insert(Point(5, 8));
+        rocks.insert(Point(5, 9));
+        rocks.insert(Point(6, 1));
+        rocks.insert(Point(6, 2));
+        rocks.insert(Point(6, 5));
+        rocks.insert(Point(6, 9));
+        rocks.insert(Point(7, 7));
+        rocks.insert(Point(7, 8));
+        rocks.insert(Point(8, 1));
+        rocks.insert(Point(8, 2));
+        rocks.insert(Point(8, 4));
+        rocks.insert(Point(8, 6));
+        rocks.insert(Point(8, 7));
+        rocks.insert(Point(8, 8));
+        rocks.insert(Point(8, 9));
+        rocks.insert(Point(9, 1));
+        rocks.insert(Point(9, 2));
+        rocks.insert(Point(9, 5));
+        rocks.insert(Point(9, 6));
+        rocks.insert(Point(9, 8));
+        rocks.insert(Point(9, 9));
+        Grid {
+            start: Point(5, 5),
+            size: 11,
             rocks,
         }
     }
 
-    fn example_initial_position() -> HashSet<usize> {
-        let mut positions = HashSet::new();
-        positions.insert(position(5, 5));
-        positions
+    fn example_state_initial() -> HashSet<Point> {
+        let mut state = HashSet::new();
+        state.insert(Point(5, 5));
+        state
     }
 
-    fn example_positions_after_one_step() -> HashSet<usize> {
-        let mut positions = HashSet::new();
-        positions.insert(position(4, 5));
-        positions.insert(position(5, 4));
-        positions
+    fn example_state_after_one_step() -> HashSet<Point> {
+        let mut state = HashSet::new();
+        state.insert(Point(4, 5));
+        state.insert(Point(5, 4));
+        state
     }
 
-    fn example_positions_after_two_steps() -> HashSet<usize> {
-        let mut positions = HashSet::new();
-        positions.insert(position(3, 5));
-        positions.insert(position(5, 5));
-        positions.insert(position(5, 3));
-        positions.insert(position(6, 4));
-        positions
-    }
-
-    #[test]
-    fn test_garden_initial_position() {
-        let garden = example_garden();
-        let initial = garden.initial_position();
-        assert_eq!(initial, example_initial_position());
+    fn example_state_after_two_steps() -> HashSet<Point> {
+        let mut state = HashSet::new();
+        state.insert(Point(3, 5));
+        state.insert(Point(5, 5));
+        state.insert(Point(5, 3));
+        state.insert(Point(6, 4));
+        state
     }
 
     #[test]
-    fn test_garden_step_from_positions() {
-        let garden = example_garden();
-        let initial = example_initial_position();
-
-        let one = example_positions_after_one_step();
-        assert_eq!(garden.step_from_positions(&initial), one);
-
-        let two = example_positions_after_two_steps();
-        assert_eq!(garden.step_from_positions(&one), two);
-    }
-
-    #[test]
-    fn test_garden_from_str() {
+    fn test_grid_from_str() {
         assert_eq!(
-            Garden::from_str(&advent_of_code::template::read_file("examples", DAY)),
-            Ok(example_garden()),
+            Grid::from_str(&advent_of_code::template::read_file("examples", DAY)),
+            Ok(example_grid()),
         );
     }
 
     #[test]
-    fn test_total_reachable_in_steps() {
-        let garden = example_garden();
-        assert_eq!(garden.total_reachable_in_steps(6), 16);
+    fn test_grid_initial_state() {
+        let grid = example_grid();
+        assert_eq!(grid.initial_state(), example_state_initial());
+    }
+
+    #[test]
+    fn test_grid_step() {
+        let grid = example_grid();
+
+        let initial = example_state_initial();
+        let one = example_state_after_one_step();
+        assert_eq!(grid.step(&initial, false), one);
+
+        let two = example_state_after_two_steps();
+        assert_eq!(grid.step(&one, false), two);
+    }
+
+    #[test]
+    fn test_reachable_in_steps() {
+        let grid = example_grid();
+        assert_eq!(grid.reachable_in_steps(6, false), 16);
     }
 
     #[test]
@@ -378,17 +283,17 @@ mod tests {
     }
 
     #[test]
-    fn test_total_reachable_on_repeated_grid() {
-        let garden = example_garden();
-        assert_eq!(garden.total_reachable_on_repeated_grid(6), 16);
-        assert_eq!(garden.total_reachable_on_repeated_grid(50), 1594);
-        assert_eq!(garden.total_reachable_on_repeated_grid(100), 6536);
-        //       assert_eq!(garden.total_reachable_on_repeated_grid(5000), 16_733_044);
+    fn test_reachable_in_steps_on_repeated_grid() {
+        let grid = example_grid();
+        assert_eq!(grid.reachable_in_steps(6, true), 16);
+        assert_eq!(grid.reachable_in_steps(10, true), 50);
+        assert_eq!(grid.reachable_in_steps(50, true), 1594);
+        assert_eq!(grid.reachable_in_steps(100, true), 6536);
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(528192461129799));
     }
 }
